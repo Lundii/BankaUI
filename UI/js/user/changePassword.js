b45dc1b8f4e16539cf9d228b8d0c3944e84dd4e8
Lundii <mondayemmanuel67@gmail.com>
2019-05-05 23:55:45 +0100

    feature(fetch): users change password [Finishes #165811971]

---
+++ UI/js/user/changePassword.js
@@ -0,0 +1,28 @@
+
+let changePassword = () => {
+    const user = JSON.parse(localStorage.getItem('ClientUser'));
+    const body = {
+      oldPassword: document.querySelector('#oldPassword').value,
+      newPassword: document.querySelector('#newPassword').value,
+      confirmPassword: document.querySelector('#confirmPassword').value,
+    };
+    const fetchData2 = {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${user.data.token}`,
+      },
+    };
+  const url2 = `http://localhost:3000/api/v1/user/${user.data.id}/changePassword`;
+  fetch(url2, fetchData2)
+  .then((res) => res.json())
+  .then(function(data) {
+    if (data.status === 200 ) {
+      console.log('Password successfully changed')
+    }
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+}
