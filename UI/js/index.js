2a6b3928b960a57f2f453d346528b7062f588b76
Lundii <mondayemmanuel67@gmail.com>
2019-05-06 22:18:12 +0100

    feature(admin): admin ct new staff [Finishes #165821610]

--- UI/js/index.js
+++ UI/js/index.js
@@ -20,37 +20,40 @@ function showAcctHistory() {
 function showStaffDetails(staff) {
   document.querySelector('#manageStaffs').style.display = 'none';
   document.querySelector('#staffDetails').style.display = 'block';
-  document.querySelector('#staffFirstName').value = staff.firstname;
-  document.querySelector('#staffLastName').value = staff.lastname;
-  document.querySelector('#staffEmail').value = staff.email;
-  document.querySelector('#staffID').value = staff.id;
+  let staffDetails = document.querySelector('#staffDetailsForm')
+  staffDetails[0].value = staff.firstname;
+  staffDetails[1].value = staff.lastname;
+  staffDetails[2].value = staff.email;
+  staffDetails[3].value = staff.id;
+  staffDetails[4].value = 'Cashier';
   document.querySelector('#staffIDHead').innerHTML = `Staff ID: ${staff.id}`;
   document.querySelector('#staffNameHead').innerHTML = `${staff.firstname} ${staff.lastname}`;
-  document.querySelector('#staffRole').value = 'Cashier';
 }
 
 function showUserDetails(account) {
   document.querySelector('#manageUsers').style.display = 'none';
   document.querySelector('#userDetails').style.display = 'block';
-  document.querySelector('#userFirstName').value = account.firstname;
-  document.querySelector('#userLastName').value = account.lastname;
-  document.querySelector('#userAcctNum').value = account.accountnumber;
+  let accountDetails = document.querySelector('#accountDetailsForm');
+  accountDetails[0].value = account.firstname;
+  accountDetails[1].value = account.lastname;
+  accountDetails[2].value = account.accountnumber;
+  accountDetails[3].value = account.owneremail;
   document.querySelector('#headAcctNum').innerHTML = account.accountnumber;
   document.querySelector('#headAcctName').innerHTML = `${account.firstname} ${account.lastname}`;
-  document.querySelector('#userEmail').value = account.owneremail;
   localStorage.setItem('accountNumberDetails', account.accountnumber);
 }
 
 function showAdminDetails(admin) {
   document.querySelector('#manageAdmins').style.display = 'none';
   document.querySelector('#adminDetails').style.display = 'block';
-  document.querySelector('#adminFirstName').value = admin.firstname;
-  document.querySelector('#adminLastName').value = admin.lastname;
-  document.querySelector('#adminEmail').value = admin.email;
-  document.querySelector('#adminID').value = admin.id;
+  let adminDetails = document.querySelector('#adminDetailsForm');
+  adminDetails[0].value = admin.firstname;
+  adminDetails[1].value = admin.lastname;
+  adminDetails[2].value = admin.email;
+  adminDetails[3].value = admin.id;
+  adminDetails[4].value = 'Admin';
   document.querySelector('#adminIDHead').innerHTML = `Admin ID: ${admin.id}`;
   document.querySelector('#adminNameHead').innerHTML = `${admin.firstname} ${admin.lastname}`;
-  document.querySelector('#adminRole').value = 'Admin';
 }
 
 function createNewAdmin() {
